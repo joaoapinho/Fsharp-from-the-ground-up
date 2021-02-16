a70eef9e839db32051b0b2009748f936a40940e6
jap <joao.pinho@altitude.com>
2021-02-16 15:51:00 +0000

    Section 10: discriminated unions, pattern matching

--- Student.fs
+++ Student.fs
@@ -9,28 +9,35 @@ type Student =
         MinScore : float
         MaxScore : float
     }
-    
+
 module Student =
-    let namePart i (s: string) =
-        let elements = s.Split(',')
-        elements.[i].Trim()
+    let nameParts (s: string) =
+        let elements = s.Split(',') 
+        match elements with
+        | [|surname; givenName|] ->
+            {| Surname = surname.Trim()
+               GivenName= givenName.Trim() |}
+        | [|surname|] ->
+            {|  Surname = surname.Trim()
+                GivenName = "(None)" |}
+        | _ ->
+            raise(System.FormatException(sprintf "Invalid name format: \"%s" s))
 
     let fromString (s: string) =
         let elements = s.Split('\t')
-        let name = elements.[0]
-        let given = namePart 1 name 
-        let sur = namePart 0 name
+        let name = elements.[0]|> nameParts 
         let id = elements.[1]
         let scores =
             elements
                     |> Array.skip 2
-                    |> Array.map  (Float.fromStringOr 50.)
+                    |> Array.map  TestResult.fromString
+                    |> Array.choose TestResult.tryEffectiveScore
         let meanScore = scores |> Array.average
         let maxScore = scores |> Array.max
         let minScore = scores |> Array.min
         {
-            Surname = sur
-            GivenName = given
+            Surname = name.Surname
+            GivenName = name.GivenName
             Id = id
             MeanScore = meanScore
             MinScore = minScore
